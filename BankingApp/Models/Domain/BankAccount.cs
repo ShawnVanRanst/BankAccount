8ae933e0c55cc2a7a1bf5a510ca450a1a8ef4e39
Shawn Van Ranst <shawn.vanranst.y7348@student.hogent.be>
2018-09-28 09:45:31 +0200

    Creëer de klasse BankAccount

---
+++ BankingApp/Models/Domain/BankAccount.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApp.Models.Domain
+{
+    public class BankAccount
+    {
+        //private string _accountNumber;
+        #region Fields
+        public const decimal WithdrawCost = 0.10M;
+        private decimal _balance;
+        #endregion
+        #region Properties
+        public string AccountNumber { get;  set; }
+
+        //public decimal GetBalance()
+        //{
+        //    return _balance;
+        //}
+
+        //private void SetBalance(decimal value)
+        //{
+        //    _balance = value;
+        //}
+
+        //public decimal Balance { get; private set; }
+
+
+        public decimal Balance
+        {
+            get
+            {
+                return _balance;
+            }
+             set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("No negative balance is allowed");
+                }
+
+                _balance = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public BankAccount()
+        {
+
+        }
+        public BankAccount(string accountNumber)
+        {
+            AccountNumber = accountNumber;
+        }
+        public BankAccount(string accountNumber, decimal balance) : this(accountNumber)
+        {
+            Balance = balance;
+        }
+        #endregion
+
+        #region Methods
+
+        public void Deposit(decimal amount)
+        {
+            Balance += amount;
+
+        }
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount + WithdrawCost;
+        }
+        #endregion
+
+    }
+}
--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using BankingApp.Models.Domain;
+using System;
 
 namespace BankingApp
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Object Initializer
+            //BankAccount anotherBA = new BankAccount()
+            //{
+            //    AccountNumber = "123-12312321-99",
+            //    Balance = 200
+            //};
+
+
+            BankAccount myBa = new BankAccount("123-12312321-99", 50);
+            Console.WriteLine($"Accountnumber is {myBa.AccountNumber} ");
+            Console.WriteLine($"WithdrawCost is {BankAccount.WithdrawCost} ");
+            Console.WriteLine($"Balance is {myBa.Balance} Euro");
+            myBa.Deposit(200);
+            Console.WriteLine($"Balance is {myBa.Balance} Euro");
+            myBa.Withdraw(50);
+            Console.WriteLine($"Balance is {myBa.Balance} Euro");
+            Console.ReadKey();//zodat console niet direct afsluit
+
+
         }
     }
 }
